1177b023e59764927082bd0c01ca0885b3b1620e
Conrad Obi <100359386+conradobi@users.noreply.github.com>
2023-03-08 14:14:06 +0100

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
+for i in range(100):
     if i == 99:
-        print("{}".format(i), end=" ")
+        print('{}'.format(i))
     else:
         print(f'{"0" + str(i) if i < 10 else i},', end=" ")
